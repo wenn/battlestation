2c47c4d8b431f55140b90ebf0c82ab6656e4c848
Nguyen Nguyen <nnguyen920@gmail.com>
2018-03-10 14:13:09 -0600

    a bit of cleanup

--- setup/macos_run.sh
+++ setup/macos_run.sh
@@ -8,7 +8,7 @@ function ensure_the_world {
 
 function ensure_brew {
   if brew --version > /dev/null 2>&1; then
-    echo ">>> brew already installed @ version $(brew --version)."
+    echo ">> brew already installed @ version $(brew --version)."
   else
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
@@ -16,7 +16,7 @@ function ensure_brew {
 
 function ensure_git {
   if git --version > /dev/null 2>&1; then
-    echo ">>> git already installed @ version $(git --version) "
+    echo ">> git already installed @ version $(git --version) "
   else
     brew install git && brew link git
   fi
@@ -27,7 +27,7 @@ function ensure_battlestation {
   local battlestation_repo=https://github.com/wenn/battlestation.git
 
   if [ -d "$battlestation_path" ]; then
-    echo ">>> $battlestation_path already downloaded."
+    echo ">> $battlestation_path already downloaded."
   else
     git clone $battlestation_repo $battlestation_path
   fi
@@ -37,7 +37,7 @@ function ensure_python2 {
   local version="2"
 
   if brew list python@$version > /dev/null 2>&1; then
-    echo ">>> python@$version already installed."
+    echo ">> python@$version already installed."
   else
     brew install python@$version \
       && brew unlink python@$version \
@@ -51,7 +51,7 @@ function ensure_ansible {
   local pip_bin="pip$python_version"
 
   if ansible --version > /dev/null 2>&1; then
-    echo ">>> ansible==$ansible_version already installed"
+    echo ">> ansible==$ansible_version already installed"
   else
     $pip_bin install ansible==$ansible_version
   fi
